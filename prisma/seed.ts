e8c796041eed6d1ae24f9010638571814ccaf786
Jason Lengstorf <jason@lengstorf.com>
2022-03-15 11:41:06 -0700

    init

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "@node-rs/bcrypt";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const hashedPassword = await bcrypt.hash("mysupergoodpassword", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email: "you@example.com",
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My first note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My second note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+try {
+  seed();
+  process.exit(0);
+} catch (error: unknown) {
+  console.error(error);
+  process.exit(1);
+}
